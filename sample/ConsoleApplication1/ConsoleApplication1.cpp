021ee8565c74d27622e57aa29fce051c73a9276d
Youngbo Shim <aram6207@kaist.ac.kr>
2022-08-11 17:10:41 +0900

    Update Lecture 25 app

--- sample/ConsoleApplication1/ConsoleApplication1.cpp
+++ sample/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,38 +4,30 @@
 #include <iostream>
 #include <Windows.h>
 #include <stdio.h>
+#include <conio.h>
 
 int main()
 {
     HANDLE handle;
+    int Tick;
+    char buffer[100] = { 0, };
+    DWORD nOfBytesRead;
     BOOL bRet;
-    DWORD dwRet;
-    UCHAR buffer[100] = { 0, };
 
     handle = CreateFile(L"\\??\\MYSAMPLE", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
 
-    bRet = ReadFile(handle, buffer, 5, &dwRet, NULL);
-    if (bRet) printf("Read: dwRet =%d\n", dwRet);
-    else printf("Read: FAIL");
+    Tick = GetTickCount();
 
-    bRet = WriteFile(handle, "HELLO", 5, &dwRet, NULL);
-    if (bRet) printf("Write: dwRet =%d\n", dwRet);
-    else printf("Read: FAIL");
+    printf("call to ReadFile..\n");
+    bRet = ReadFile(handle, buffer, 5, &nOfBytesRead, NULL);
+    printf("returned from ReadFile..\n");
 
-    bRet = ReadFile(handle, buffer, 100, &dwRet, NULL);
-    if (bRet) printf("Read: dwRet =%d\n", dwRet);
-    else printf("Read: FAIL");
+    Tick = GetTickCount() - Tick;
+    printf("delay time = &d msec, ReadFile\n", Tick);
 
     CloseHandle(handle);
+
+    _getch();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
